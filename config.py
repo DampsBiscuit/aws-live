8bc0cb8a6f1ee48dcfbaddc635fa1ad5dbd42d00
DampsBiscuit <167176851+DampsBiscuit@users.noreply.github.com>
2024-04-16 15:47:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cxc264skibv1.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chooiweizhern-employee"
+customregion = "us-east-1"
+
